5e806fcc39882a1c0998dfa5f2fd19b72db00526
David Lu <davidzlu@hotmail.com>
2016-07-11 15:41:39 -0700

    Added password files

---
+++ basic_game/game.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {
+  //var panlex = require("panlex");
+
+  // Send data to server function, use JSON format
+  // Pull data from server, possibly use panlex JS library
+  // Painting and display/screen transition function
+  // Need to go from language name to uid, or have people enter uid
+  // curl http://api.panlex.org/ex -d {fill in with language requests}
+  
+  var placeholderData = { 
+    "resultType": "ex",
+    "result": [],
+    "resultNum": 0,
+    "resultMax": 2000
+  }; //TODO: Figure out format data will come in as
+  var placeholderInput = {
+    "uid": targetLanguage,
+    "trtt": "userTranslation",
+    // Other parameters sent
+  };
+  var placeholderAlgoData = {};
+
+  var activityType = "translate";
+  var translationData = placeholderData,
+      translationInput = placeholderInput;
+  
+  var sourceLanguage, targetLanguage; // Assigned by user input
+  var nextWord = "Hello"; // Tracks next word to translate. Maybe an int? 
+
+  var curScreen = $("#start");
+  var ALGORITHM_COUNT = 2; // Number of algorithms to evaluate
+  var algoList = [1, 2]
+
+
+  function createQuestionElement() {
+    /* Dynamically creates form for accepting user input. */
+    var question = $('<div id="question">');
+    if (activityType == "translate") {
+      var questionTitle = $('<h1>Translate</h1>'),
+      sourceHeader = $('<h3>' + sourceLanguage + '</h3>'),
+      sourceWord = $('<p>' + nextWord + '</p>'),
+      targetHeader = $('<h3>' + targetLanguage + '</h3>'),
+      form = createFormElement();
+    } else {
+      var questionTitle = $('<h1>Evaluate</h1>'),
+      sourceHeader = $('<h3>' + sourceLanguage + '</h3>'),
+      sourceWord = $('<p>' + nextWord + '</p>'),
+      targetHeader = $('<h3>' + targetLanguage + '</h3>'),
+      form = createRadioElement();
+    }
+    question.append(questionTitle, sourceHeader, sourceWord, targetHeader, form);
+    return question;
+  }
+
+  function createFormElement() {
+    /* Helper function for createQuestionElement. Creates an HTML form
+       element that accepts text input. */
+    var form = $('<form name="userTranslation" action="receiver.py" method="post">');
+    form.append('<input type="text" name="translation"><br/>');
+    form.append('<button type="button" name="submit">Submit</button>');
+    return form
+  }
+
+  function createRadioElement() {
+    /* Helper function for createQuestionInput. Creates a div element that
+       contains radio buttons. */
+    var radioList = $('<div id="radioList">');
+    for (var i=0; i < ALGORITHM_COUNT; i++) {
+      var item = $('<input type="radio" name="translation">' + algoList[i] + '</input><br/>');
+      radioList.append(item);
+    }
+    radioList.append('<button type="button" name="submit">Submit</button>');
+    return radioList;
+  }
+
+  function createQuery() {
+    /* Helper function for displayQuestion. Parses language input form
+       and sets up a query for PanLex API using AJAX. */
+    var xhttp;
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    } else {
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    var panlex_url = "http://api.panlex.org/ex";
+    var languages = $("form[name=languageSelect] input");
+    sourceLanguage = languages[0].value;
+    targetLanguage = languages[1].value;
+  //   var parameters = {
+  //     "uid": "rus-000",
+  //     "trtt": "tree",
+  //     "indent": true
+  //   };
+  //   var query = { 
+  //     data: parameters,
+  //     dataType: "json",
+  //     method: "POST",
+  //     url: panlex_url
+  //   };
+  //   var results = $.ajax(query)
+  //     .done(function() {
+  //       alert("success");
+  //     })
+  //     .fail(function() {
+  //       alert("fail");
+  //     });
+  //   console.log(results);
+
+  //   panlex.query("/ex", parameters, function(err, data) {
+  //     alert("success");
+  //   });
+  //   return results;
+  }
+
+  function displayLanguages() {
+    /* Handles transition from start screen to language input screen. */
+    curScreen.fadeOut(function() {
+      // Set game type
+      curScreen = $("#languages");
+      curScreen.fadeIn();
+    });
+  }
+
+  function displayQuestion() {
+    /* Handles transition from language input screen to user input screen. */
+    curScreen.fadeOut(function() {
+      createQuery();
+      curScreen = $("#translate");
+      $("#question").remove();
+      var question = createQuestionElement();
+      curScreen.append(question).fadeIn();
+      $("button[name=submit]").on("click", displayEnd);
+    });
+  }
+
+  function displayEnd() {
+    /* Handles transition from translation data input to end screen. */
+    curScreen.fadeOut(function() {
+      // Send data to server
+      curScreen = $("#end");
+      curScreen.fadeIn();
+    });
+  }
+
+  $("#start button").on("click", displayLanguages);
+  $("#languages button").on("click", displayQuestion);
+  $("#end:last-child").on("click", displayQuestion);
+});
---
+++ password/password.js
@@ -0,0 +1,204 @@
+$(document).ready(function() {
+  
+  var sourceLanguage; // Assigned by user input
+  var curScreen = $("#start");
+  var role = "password";
+  var password = null;
+  var guess = "hello";
+
+  function getGuess() {
+    // Should retrieve guess from server
+    return guess;
+  }
+
+  function getClue() {
+    // Should retrieve clue from server
+    return "none";
+  }
+
+  function createFormElement(inputName, formAction="") {
+    /* Helper function for createQuestionElement. Creates an HTML form
+       element that accepts text input. */
+    var form = $('<form>', { name: "userInput",
+      action: formAction,
+      method: "post"});
+    var input = $('<input>', {type: "text", name: inputName});
+    var submitButton = $('<button type="button" name="submit">Submit</button>');
+    form.append(input, '<br/>', submitButton);
+    return form
+  }
+
+  function createPasswordForm() {
+    
+    var passElem = $("<div>", { id: "passEntry"});
+    var passFormTitle = $("<h2>Enter a password</h2>");
+    var passForm = createFormElement("password");
+    passElem.append(passFormTitle, passForm);
+    return passElem;
+  }
+
+  function createClueForm() {
+    var clueElem = $('<div>', { id: "clueEntry"});
+    var clueFormTitle = $("<h2>Enter a clue</h2>");
+    var clueForm = createFormElement("clue");
+    clueElem.append(clueFormTitle, clueForm);
+    return clueElem;
+  }
+
+  function createWaitScreen() {
+    $("#wait").remove();
+    var waitScreen = $('<div>', { id: "wait"});
+    var message, callback;
+    if (role == "password") {
+      message = $("<p>Waiting for guess...</p>");
+      callback = displayPassword;
+    } else if (role == "guess") {
+      message = $("<p>Waiting for clue...</p>");
+      callback = displayGuess;
+    }
+    waitScreen.append(message);
+    message.on("click", callback);
+    return waitScreen;
+  }
+
+  function createGuessForm() {
+    $("#guessEntry").remove();
+    var guessEntry = $('<div>', { id: "guessEntry"});
+    var title = $("<h2>Type in a guess</h2>");
+    var clue = $("<p>Clue: " + getClue() + "</p>");
+    var form = createFormElement("guessForm");
+    guessEntry.append(title, clue, form);
+    return guessEntry;
+  }
+
+  function createSwapScreen() {
+    $("#swapContainer").remove();
+    var swapScreen = $("<div>", { id: "swapContainer"});
+    var message = $("<p>Switching roles...</p>");
+    var swapButton = $("<button>Switch</button>", { type:"button", name:"swapButton"});
+    if (role == "password") {
+      role = "guess";
+    } else if (role == "guess") {
+      role = "password";
+    }
+    swapScreen.append(message, swapButton);
+    swapButton.on("click", displayPlayScreen);
+    return swapScreen;
+  }
+
+  function displayPassword() {
+    curScreen.fadeOut(function() {
+      switch (curScreen.attr("id")) {
+        case "matching":
+          curScreen = $("#password");
+          curScreen.append(createPasswordForm()).fadeIn();
+          break;
+        case "password":
+          if (password == null) {
+            password = $('form[name=userInput] input')[0].value;
+          }
+          curScreen = $("#clue");
+          $('#password').empty();
+          var endButton = $('<button type="button" name="endButton">End Round</button>');
+          curScreen.append(createClueForm(), endButton).fadeIn();
+          $('button[name=endButton]').on("click", function() {
+            guess = password;
+            displayPassword();
+          });
+          break;
+        case "clue":
+          curScreen = $("#waiting");
+          $('#clue').empty();
+          curScreen.append(createWaitScreen(), endButton).fadeIn();
+          break;
+        case "waiting":
+          if (guess == password) {
+            curScreen = $("#swap");
+            curScreen.append(createSwapScreen()).fadeIn();
+          } else {
+            curScreen = $("#password");
+            displayPassword();
+          }
+          break;
+        default:
+          curScreen = $("#matching");
+          guess = "hello"
+          displayPassword();
+      }
+    });
+  }
+
+  function displayLanguages() {
+    /* Handles transition from start screen to language input screen. */
+    curScreen.fadeOut(function() {
+      curScreen = $("#languages");
+      curScreen.fadeIn();
+    });
+  }
+
+  function displayGuess() {
+    curScreen.fadeOut(function() {
+      switch (curScreen.attr("id")) {
+        case "matching":
+          curScreen = $("#waiting");
+          curScreen.append(createWaitScreen()).fadeIn();
+          break;
+        case "waiting":
+          curScreen = $("#guess");
+          $("button[name=endButton]").remove();
+          var endButton = $('<button type="button" name="endButton">End Round</button>');
+          curScreen.append(createGuessForm(), endButton).fadeIn();
+          $('button[name=endButton]').on("click", function() {
+            guess = password;
+            displayGuess();
+          });
+          break;
+        case "guess":        
+          if (guess == password) {
+            curScreen = $("#swap");
+            curScreen.append(createSwapScreen()).fadeIn();
+          } else {
+            curScreen = $("#matching");
+            displayGuess();
+          }
+          break;
+        default:
+          curScreen = $("#matching");
+          guess = "hello"
+          displayGuess();
+      }
+    });
+  }
+
+  function displayPlayScreen() {
+    // Manages selection b/t displayPassword and displayGuess
+    if (role == "password") {
+      displayPassword();
+    } else if (role == "guess") {
+      displayGuess();
+    }
+  }
+
+  function displayMatching() {
+    curScreen.fadeOut(function() {
+      curScreen = $("#matching");
+      curScreen.append('<p>Matching...</p>');
+      $('p').on("click", displayPlayScreen);
+      curScreen.fadeIn();
+    });
+  }
+
+  function displayEnd() {
+    /* Handles transition from translation data input to end screen. */
+    curScreen.fadeOut(function() {
+      // Send data to server
+      curScreen = $("#end");
+      curScreen.fadeIn();
+    });
+  }
+
+  $("#start button").on("click", displayLanguages);
+  $("#languages button").on("click", displayMatching);
+  $("#container").on("click", "button[name=submit]", displayPlayScreen);
+  $("#end:last-child").on("click", displayPlayScreen);
+});
