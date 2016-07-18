cb315c82a731378d703dc36d83579a2ef3e2c2d3
David Lu <davidzlu@hotmail.com>
2016-07-18 14:20:30 -0700

    Changed directory structure, basic server working

--- password/server.js
+++ password/server.js
@@ -1,17 +1,38 @@
-var app = require("http").createServer(handler);
-var io = require("socket.io")(app);
+var http = require("http");
+var socketio = require("socket.io");
+var express = require("express");
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
 var PORT = 8000;
 
-io.on('connection', onConnection);
+var waitingPlayer;
 
-function handler(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}
-
-app.listen(PORT, function() {
+app.use(express.static(__dirname + '/client'));
+server.listen(PORT, function() {
     console.log('Server running at http://127.0.0.1:' + PORT.toString() + '/');
 });
 
+io.on('connection', onConnection);
+
+function onConnection(sock) {
+    sock.emit('msg', 'Hello, You are playing Password!');
+    sock.on('password', function(pword) {
+        // Refer to game logic file
+    });
+    sock.on('guess', function() {
+        // Refer to game logic file
+    });
+    sock.on('clue', function() {
+        // Refer to game logic file
+    });
+
+    if (waitingPlayer) {
+        waitingPlayer = null;
+    } else {
+        waitingPlayer = sock;
+        sock.emit('msg', 'Matching you with another player'); // TODO: change event to move client to matching screen
+    }
+}
 
