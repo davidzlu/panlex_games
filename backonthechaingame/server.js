d7b8ce27133ff1475633c7eecf2c3a33fb3b4e96
Grace O'Hair-Sherman <grace.ohair.sherman@gmail.com>
2016-08-02 10:31:22 -0700

    connected client.js to index.html

--- backonthechaingame/server.js
+++ backonthechaingame/server.js
@@ -3,21 +3,13 @@ var socketio = require("socket.io");
 var express = require("express");
 var url = require('url');
 var fs = require('fs');
-//var PasswordGame = require("./PasswordGame");
-
 var app = express();
-//var server = http.createServer(app);
 var io = socketio(server);
 var PORT = 8000;
 
 app.use(express.static(__dirname + '/client'));
-//server.listen(PORT, function() {
-  //console.log('Server running at http://127.0.0.1:' + 
-//PORT.toString() + '/');
-//});
 
 var server = http.createServer(app,function(request, response){
-
     var path = url.parse(request.url).pathname; 
     fs.readFile(__dirname +path, function(error, data){
     });
@@ -25,4 +17,5 @@ var server = http.createServer(app,function(request, response){
         response.write(data, "utf8");
         response.end();
 });
+
 server.listen(8000);
