4723e856c986672f8c5efc2c24d06444e9ebbd08
David Lu <davidzlu@hotmail.com>
2016-08-08 16:53:19 -0700

    Modified for dropdown, cleaned up client.js

--- backonthechaingame/client/js/client.js
+++ backonthechaingame/client/js/client.js
@@ -6,29 +6,9 @@ $(document).ready(function() {
     var language;
 
     socket.on("languageSuccess", onLanguageSuccess);
-    socket.on("languageFail", function(msg) {
-        console.log(msg);
-        alert(msg);
-    });
-    socket.on("sendWords", function(word1, word2) {
-        console.log("received "+word1+" and "+word2);
-        var translationButton = $("<button>",{ type:"button", name:"seeTrans",text:"Show translations"});
-        translationButton.on("click", function(){
-            console.log("translation button clicked, asking server for translations");
-            socket.emit("askTrans",$("#selectSecondaryLang").val(),word1);  //asks server for a translation of word1
-        });
-        $("#gameContainer h2").after(translationButton);
-        $("#gameContainer h2").after("Choose a language in which to list PanLex's translations/synonyms of <font color=\"FF0000\">"+word1+"</font>: <br>");
-        $("#gameContainer h2").after("<h3>Can you get from <font color=\"FF0000\">"+word1+"</font> to <font color=\"FF0000\">"+word2+"</font> using a chain of translations/synonyms?</h3>");
-        //curScreen.append("<h3>Can you get from <font color=\"FF0000\">"+word1+"</font> to <font color=\"FF0000\">"+word2+"</font> using a chain of translations/synonyms?</h3>");
-        //curScreen.append("Choose a language in which to list PanLex's translations/synonyms of <font color=\"FF0000\">"+word1+"</font>: <br>");
-       
-
-    });
-    socket.on("singleTrans", function(word){
-        console.log(word);
-        curScreen.append("<br>"+word);
-    });
+    socket.on("languageFail", onLanguageFail);
+    socket.on("sendWords", onReceiveWords);
+    socket.on("singleTrans", onReceiveTranslation);
 
     /* When the button on the first screen is clicked, send language 
      * selection to server and show second screen */
@@ -47,5 +27,30 @@ $(document).ready(function() {
             curScreen.append().fadeIn();
             $("#currentLanguage").text(msg);
         });
+    }
+
+    function onLanguageFail(msg) {
+        console.log(msg);
+        alert(msg);
+    }
+
+    function onReceiveWords(word1, word2) {
+        console.log("received "+word1+" and "+word2);
+        var translationButton = $("<button>",{ type:"button", name:"seeTrans",text:"Show translations"});
+        translationButton.on("click", function(){
+            console.log("translation button clicked, asking server for translations");
+            socket.emit("askTrans", $("#selectSecondaryLang").val(), word1);  //asks server for a translation of word1
+        });
+        $("#selectSecondaryLang").after(translationButton);
+        $("#gameContainer h2").after("Choose a language in which to list PanLex's translations/synonyms of <font color=\"FF0000\">"+word1+"</font>: <br>");
+        $("#gameContainer h2").after("<h3>Can you get from <font color=\"FF0000\">"+word1+"</font> to <font color=\"FF0000\">"+word2+"</font> using a chain of translations/synonyms?</h3>");
+        //curScreen.append("<h3>Can you get from <font color=\"FF0000\">"+word1+"</font> to <font color=\"FF0000\">"+word2+"</font> using a chain of translations/synonyms?</h3>");
+        //curScreen.append("Choose a language in which to list PanLex's translations/synonyms of <font color=\"FF0000\">"+word1+"</font>: <br>");
+    }
+
+    function onReceiveTranslation(word) {
+        console.log(word);
+        var $wordOption = $("<option>"+word+"<option/>")
+        $("#synonymContainer").append($wordOption);
     }
 });
